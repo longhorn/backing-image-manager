e392670858f950db8ba0db83b97117d97c1d157e
Shuo Wu <shuo.wu@suse.com>
2021-08-18 22:39:10 +0800

    datasource: support exporting a volume to a backing image
    Longhorn 2403
    
    Signed-off-by: Shuo Wu <shuo.wu@suse.com>

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,6 +1,9 @@
 package util
 
 import (
+	"bufio"
+	"bytes"
+	"context"
 	"crypto/sha512"
 	"encoding/hex"
 	"encoding/json"
@@ -8,11 +11,19 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 
 	"github.com/longhorn/sparse-tools/sparse"
+)
+
+const (
+	QemuImgBinary = "qemu-img"
 )
 
 func PrintJSON(obj interface{}) error {
@@ -119,4 +130,102 @@ func ReadBackingImageConfigFile(workDirectory string) (*BackingImageConfig, erro
 		return nil, errors.Wrapf(err, "failed to unmarshal %v content %v", filePath, output)
 	}
 	return cfg, nil
+}
+
+func Execute(envs []string, binary string, args ...string) (string, error) {
+	return ExecuteWithTimeout(time.Minute, envs, binary, args...)
+}
+
+func ExecuteWithTimeout(timeout time.Duration, envs []string, binary string, args ...string) (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	var err error
+	cmd := exec.CommandContext(ctx, binary, args...)
+	cmd.Env = append(os.Environ(), envs...)
+	done := make(chan struct{})
+
+	var output, stderr bytes.Buffer
+	cmd.Stdout = &output
+	cmd.Stderr = &stderr
+
+	go func() {
+		err = cmd.Run()
+		done <- struct{}{}
+	}()
+
+	select {
+	case <-done:
+	case <-ctx.Done():
+		if cmd.Process != nil {
+			if err := cmd.Process.Kill(); err != nil {
+				logrus.Warnf("problem killing process pid=%v: %s", cmd.Process.Pid, err)
+			}
+		}
+		return "", fmt.Errorf("timeout executing: %v %v, output %s, stderr, %s, error %v",
+			binary, args, output.String(), stderr.String(), err)
+	}
+
+	if err != nil {
+		return "", fmt.Errorf("failed to execute: %v %v, output %s, stderr, %s, error %v",
+			binary, args, output.String(), stderr.String(), err)
+	}
+	return output.String(), nil
+}
+
+func DetectFileFormat(filePath string) (string, error) {
+
+	/* Example command outputs
+	   $ qemu-img info parrot.raw
+	   image: parrot.raw
+	   file format: raw
+	   virtual size: 32M (33554432 bytes)
+	   disk size: 2.2M
+
+	   $ qemu-img info parrot.qcow2
+	   image: parrot.qcow2
+	   file format: qcow2
+	   virtual size: 32M (33554432 bytes)
+	   disk size: 2.3M
+	   cluster_size: 65536
+	   Format specific information:
+	       compat: 1.1
+	       lazy refcounts: false
+	       refcount bits: 16
+	       corrupt: false
+	*/
+
+	output, err := Execute([]string{}, QemuImgBinary, "info", filePath)
+	if err != nil {
+		return "", err
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(output))
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if strings.HasPrefix(line, "file format: ") {
+			return strings.TrimPrefix(line, "file format: "), nil
+		}
+	}
+
+	return "", fmt.Errorf("cannot find the file format in the output %s", output)
+}
+
+func ConvertFromRawToQcow2(filePath string) error {
+	if format, err := DetectFileFormat(filePath); err != nil {
+		return err
+	} else if format == "qcow2" {
+		return nil
+	}
+
+	tmpFilePath := filePath + ".qcow2tmp"
+	defer os.RemoveAll(tmpFilePath)
+
+	if _, err := Execute([]string{}, QemuImgBinary, "convert", "-f", "raw", "-O", "qcow2", filePath, tmpFilePath); err != nil {
+		return err
+	}
+	if err := os.RemoveAll(filePath); err != nil {
+		return err
+	}
+	return os.Rename(tmpFilePath, filePath)
 }
