18e26ae6d778bdb0eeac7f7efbf03ad04660a978
Shuo Wu <shuo.wu@suse.com>
2022-06-02 11:15:24 +0800

    Add pprof service
    Signed-off-by: Shuo Wu <shuo.wu@suse.com>

--- pkg/sync/router.go
+++ pkg/sync/router.go
@@ -1,6 +1,8 @@
 package sync
 
 import (
+	"net/http/pprof"
+
 	"github.com/gorilla/mux"
 )
 
@@ -22,6 +24,18 @@ func NewRouter(service *Service) *mux.Router {
 	router.HandleFunc("/v1/files", service.DownloadFromURL).Methods("POST").Queries("action", "downloadFromURL")
 	router.HandleFunc("/v1/files", service.UploadFromRequest).Methods("POST").Queries("action", "upload")
 	router.HandleFunc("/v1/files", service.ReceiveFromPeer).Methods("POST").Queries("action", "receiveFromPeer")
+
+	router.HandleFunc("/debug/pprof/", pprof.Index).Methods("GET")
+	router.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline).Methods("GET")
+	router.HandleFunc("/debug/pprof/profile", pprof.Profile).Methods("GET")
+	router.HandleFunc("/debug/pprof/symbol", pprof.Symbol).Methods("GET")
+	router.HandleFunc("/debug/pprof/trace", pprof.Trace).Methods("GET")
+	router.Handle("/debug/pprof/allocs", pprof.Handler("allocs")).Methods("GET")
+	router.Handle("/debug/pprof/block", pprof.Handler("block")).Methods("GET")
+	router.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine")).Methods("GET")
+	router.Handle("/debug/pprof/heap", pprof.Handler("heap")).Methods("GET")
+	router.Handle("/debug/pprof/mutex", pprof.Handler("mutex")).Methods("GET")
+	router.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate")).Methods("GET")
 
 	return router
 }
