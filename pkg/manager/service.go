7895d140980f7217b1e7efff7ec3ac55152b6523
Chin-Ya Huang <chin-ya.huang@suse.com>
2025-06-11 11:46:08 +0800

    chore(import): regroup
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- pkg/manager/service.go
+++ pkg/manager/service.go
@@ -11,15 +11,16 @@ import (
 	"time"
 
 	"github.com/golang/protobuf/ptypes/empty"
-	butil "github.com/longhorn/backupstore/util"
-	lhbitmap "github.com/longhorn/go-common-libs/bitmap"
-	rpc "github.com/longhorn/types/pkg/generated/bimrpc"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
+
+	butil "github.com/longhorn/backupstore/util"
+	lhbitmap "github.com/longhorn/go-common-libs/bitmap"
+	rpc "github.com/longhorn/types/pkg/generated/bimrpc"
 
 	"github.com/longhorn/backing-image-manager/api"
 	"github.com/longhorn/backing-image-manager/pkg/backup"
