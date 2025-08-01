9b8f3f44b1d765cc7c437f6de13d04ed9263bcc8
Raphanus Lo <yunchang.lo@suse.com>
2025-08-01 11:38:37 +0800

    fix: enlarge io context timeout to avoid endless restart loop
    longhorn-11309
    
    Signed-off-by: Raphanus Lo <yunchang.lo@suse.com>
    (cherry picked from commit f5cae3ee16c9c2ba6426633b1e56fc8c5ad11a14)

--- pkg/sync/handler.go
+++ pkg/sync/handler.go
@@ -129,6 +129,7 @@ func IdleTimeoutCopy(ctx context.Context, cancel context.CancelFunc, src io.Read
 			case <-ctx.Done():
 				done = true
 			case <-t.C:
+				logrus.WithField("HTTPTimeout", types.HTTPTimeout.Seconds()).Error("IO timeout exceeded, cancel the copy")
 				cancel()
 				done = true
 			case _, writeChOpen := <-writeSeekCh:
