d6d41245f18812c7f1f702c2a058e57eb7def5e0
Derek Su <derek.su@suse.com>
2026-08-06 20:04:33 +0800

    fix: update github.com/urfave/cli to v3
    Signed-off-by: Derek Su <derek.su@suse.com>

--- app/cmd/start.go
+++ app/cmd/start.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 
 	"github.com/sirupsen/logrus"
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v3"
 
 	"github.com/longhorn/backing-image-manager/pkg/manager"
 	filesync "github.com/longhorn/backing-image-manager/pkg/sync"
@@ -14,39 +14,40 @@ import (
 	"github.com/longhorn/backing-image-manager/pkg/util"
 )
 
-func StartCmd() cli.Command {
-	return cli.Command{
+func StartCmd() *cli.Command {
+	return &cli.Command{
 		Name: "daemon",
 		Flags: []cli.Flag{
-			cli.StringFlag{
+			&cli.StringFlag{
 				Name:  "listen",
 				Value: "localhost:" + strconv.Itoa(types.DefaultManagerPort),
 				Usage: "Specify the manager server endpoint to listen on host:port. Defaults to localhost:8000",
 			},
-			cli.StringFlag{
+			&cli.StringFlag{
 				Name:  "sync-listen",
 				Value: "localhost:" + strconv.Itoa(types.DefaultSyncServerPort),
 				Usage: "Specify the sync server endpoint to listen on host:port. Defaults to localhost:8001",
 			},
-			cli.StringFlag{
+			&cli.StringFlag{
 				Name:  "disk-uuid",
 				Usage: "The corresponding disk uuid stored in the metafile of the disk path",
 			},
-			cli.StringFlag{
+			&cli.StringFlag{
 				Name:  "port-range",
 				Value: "30001-31000",
 				Usage: "The port is used for starting temporary sparse file server when syncing backing image, Defaults to 30001-31000",
 			},
 		},
-		Action: func(c *cli.Context) {
+		Action: func(ctx context.Context, c *cli.Command) error {
 			if err := start(c); err != nil {
 				logrus.WithError(err).Fatalf("Error running start command")
 			}
+			return nil
 		},
 	}
 }
 
-func start(c *cli.Context) error {
+func start(c *cli.Command) error {
 	listen := c.String("listen")
 	syncListen := c.String("sync-listen")
 	diskUUID := c.String("disk-uuid")
